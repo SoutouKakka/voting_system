85eb14af8a77b97683515c328f081df59ab890fd
soutoukakka <fritz@aftership.com>
2019-07-29 11:12:04 +0800

    Add production config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,13 @@
+const _ = require('lodash');
+
 const developmentConfig = require('./development');
+const productionConfig = require('./production');
 
-const config = developmentConfig;
+const config = {
+	development: developmentConfig,
+	production: _.merge(_.cloneDeep(developmentConfig), productionConfig)
+};
 
-module.exports = config;
+const env = process.env.NODE_ENV || 'development';
+
+module.exports = config[env];
